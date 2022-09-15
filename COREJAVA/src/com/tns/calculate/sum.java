0a86ed906c77d75c0e9aef4aca078fb962aef35a
Admin <Admin@DESKTOP-LP5AK88>
2022-09-15 00:19:49 -0700

    Second Commit

---
+++ COREJAVA/src/com/tns/calculate/sum.java
@@ -0,0 +1,16 @@
+package com.tns.calculate;
+
+
+public class sum 
+{
+
+		int a=20;
+		int b=30;
+		
+		public void calculate()
+		{
+			int s =a+b;
+			System.out.println("sum:" +s);
+		}
+	
+}
---
+++ COREJAVA/src/com/tns/result/sumtest.java
@@ -0,0 +1,11 @@
+package com.tns.result;
+import com.tns.calculate.*;
+
+public class sumtest {
+	public static void main (String[] args)
+	{
+		sum s= new sum ();
+		s.calculate ();
+	}
+
+}
