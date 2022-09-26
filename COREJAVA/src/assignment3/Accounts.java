cae11973f4c13ef657356491cd59c7a7ecc58d74
Admin <Admin@DESKTOP-LP5AK88>
2022-09-26 23:38:53 +0530

    First Commit

---
+++ COREJAVA/src/assignment3/Accounts.java
@@ -0,0 +1,6 @@
+package assignment3;
+
+public interface Accounts 
+{
+	void deliverycharge();
+}
---
+++ COREJAVA/src/assignment3/Non_prime.java
@@ -0,0 +1,11 @@
+package assignment3;
+
+public class Non_prime implements Accounts
+{
+	public void deliverycharge()
+	{
+		int charges=100;
+		System.out.println("The delivery "
+				+ "charge for non prime will be" +charges);
+	}
+}
